2d28dd38aa70242f23402442c90b4385e537580f
gottajsx <gottajsx@gmail.com>
2025-03-01 19:01:01 +0100

    Styling for StackNavigator

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Text } from "react-native";
 
 const StackNavigator = () => {
@@ -10,14 +11,38 @@ const StackNavigator = () => {
 
     const BottomTabs = () => {
         return(
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions = {() => ({
+                    tabBarShowLabel: false,
+                    tabBarStyle: {height: 90},
+            })}>
                 <Tab.Screen 
                     name="Home" 
                     component={HomeScreen}
+                    options={{
+                        tabBarStyle: {backgroundColor: '#101010'},
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Ionicons
+                                    style={{ paddingTop: 3 }}
+                                    name="shuffle-outline"
+                                    size={30}
+                                    color="white"
+                                />
+                            ) : (
+                                <Ionicons
+                                    style={{ paddingTop: 3 }}
+                                    name="shuffle-outline"
+                                    size={30}
+                                    color="#989898"
+                                />
+                            ),
+                    }}
                 />
             </Tab.Navigator>
         );
-    };
+    }
 
     const AuthStack = () => {
         return(
