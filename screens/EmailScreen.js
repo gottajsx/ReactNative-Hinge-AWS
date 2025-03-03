b06060491235a881d52cbdfa52ed0580eba0bd8a
gottajsx <gottajsx@gmail.com>
2025-03-03 22:13:10 +0100

    Starting EmailScreen.js

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -1,4 +1,15 @@
-import { Text } from "react-native";
+import {
+    StyleSheet, 
+    Text, 
+    View,
+    SafeAreaView,
+    Platform,
+    TouchableOpacity,
+    Image,
+    TextComponent
+} from "react-native";
+import React, {useState, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const EmailScreen = () => {
     return(
